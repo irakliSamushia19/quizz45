9cd133da83b645e992850d7210958f603e554300
irakliSamushia19 <irakli.samushia.19@gmail.com>
2025-01-31 00:23:30 +0400

    doit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer style={{ textAlign: 'center', marginTop: '50px', padding: '20px', backgroundColor: '#f1f1f1' }}>
+        <footer style={{ textAlign: 'center', marginTop: '50px', padding: '20px', backgroundColor: '#0048BA' }}>
             <p>&copy; {new Date().getFullYear()} React App</p>
         </footer>
     );
